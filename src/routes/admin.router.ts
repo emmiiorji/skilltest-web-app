cf342d8a0d967d6168e6524dc6dc3a4f7be5c6e4
emmiiorji <emmanuelorji2013@gmail.com>
2025-05-21 09:28:02 +0100

    Register create test and ai export controllers

--- src/routes/admin.router.ts
+++ src/routes/admin.router.ts
@@ -2,6 +2,8 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { groupController } from '../controllers/group.controller';
 import { profileController } from '../controllers/profile.controller';
 import { testController } from '../controllers/test.controller';
+import { testCreateController } from '../controllers/testCreate.controller';
+import { aiExportController } from '../controllers/aiExport.controller';
 import { env } from '../env.config';
 
 const authenticateAdmin = async (request: FastifyRequest, reply: FastifyReply) => {
@@ -15,6 +17,8 @@ export default async function adminRouter(fastify: FastifyInstance) {
   fastify.addHook('preHandler', authenticateAdmin);
 
   fastify.register(profileController, { prefix: '/profile' });
-  fastify.register(testController, { prefix: '/test' });
   fastify.register(groupController, { prefix: '/group' });
+  fastify.register(testController, { prefix: '/test' });
+  fastify.register(testCreateController, { prefix: '/test' });
+  fastify.register(aiExportController, { prefix: '/test' });
 }
